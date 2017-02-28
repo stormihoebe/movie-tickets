02b7ecdfcdbc1298787467b85a6683b16cc25183
Evan Bishop and Stormi Hoebelheinrich <stormi@>
2017-02-28 11:25:59 -0800

    add a website that will accept user input and print out ticket price using object constructor and prototype

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+
+function Movie(name, age, time){
+this.name = name;
+this.age = age;
+this.time = time;
+};
+var movieInput;
+var ageInput;
+var timeInput;
+var movieInputObject;
+var price;
+Movie.prototype.Ticket = function() {
+	if (ageInput === "Senior"){
+		price = "$30"
+	}else{
+		price = "$50"
+	};
+	$("#result").html("Movie Name: " + movieInput +"<br>"+ " Age Range: " + ageInput + "<br>"+" Time of Day: " + timeInput + "<br>"+" Your ticket price is " + price);
+};
+$(function(){
+
+$("#movieinfo").submit(function(event){
+	event.preventDefault();
+	movieInput = $("#movieName").val();
+	ageInput = $("#age").val();
+	timeInput =$("#timeOfDay").val();
+
+movieInputObject = new Movie(movieInput, ageInput, timeInput);
+console.log(ageInput);
+movieInputObject.Ticket();
+
+}); //submit function
+
+
+
+
+
+
+});
